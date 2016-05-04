2f51f3d7a1e21dd9faeaf5feb3c335eb98108bde
Adrian Tangochin <tangochin@yandex.ru>
2016-05-04 15:41:42 +0500

    Relative imports.

--- example/app/views.py
+++ example/app/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from random import randint
 
 from django.views.generic import TemplateView
-from models import Goods, Order
+from .models import Goods, Order
 from yandex_money.forms import PaymentForm
 from yandex_money.models import Payment
 
