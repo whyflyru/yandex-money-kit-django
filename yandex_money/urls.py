28c96558f4f88db92976591a5c7a3397cff3fcc2
buryi <buryi.d@gmail.com>
2016-10-20 17:54:12 +0700

    Django 1.8 urls style

--- yandex_money/urls.py
+++ yandex_money/urls.py
@@ -6,7 +6,7 @@ from .views import NoticeFormView
 from .views import CheckOrderFormView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^check/$', CheckOrderFormView.as_view(), name='yandex_money_check'),
     url(r'^aviso/$', NoticeFormView.as_view(), name='yandex_money_notice'),
-)
+]
