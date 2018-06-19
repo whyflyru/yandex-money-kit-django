0e422ec259ff66fafd76075b5d9c8475e96be848
Ivan Gureev <dev2@topnomer.ru>
2018-06-19 10:39:38 +0300

    Fix migrations for existing applications

---
+++ yandex_money/migrations/0003_alter_payment_fields.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('yandex_money', '0002_auto_20151214_1640'),
+    ]
+
+    operations = [
+    ]
